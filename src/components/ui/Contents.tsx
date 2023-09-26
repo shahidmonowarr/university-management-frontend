7da9b7e1a1e1f3726340c0a976efc9c8e2b4c037
shahidmonowarr <shahidmonowar21@gmail.com>
2023-09-26 22:31:32 +0600

    reausable input Field & breadCrumb, google font added

--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { Layout } from "antd";
+import UMBreadCrumb from "./UMBreadCrumb";
 const { Content } = Layout;
 
 const Contents = ({ children }: { children: React.ReactNode }) => {
+  const base = "admin";
   return (
     <Content
       style={{
@@ -10,6 +12,18 @@ const Contents = ({ children }: { children: React.ReactNode }) => {
         color: "black",
       }}
     >
+      <UMBreadCrumb
+        items={[
+          {
+            label: `${base}`,
+            link: `/${base}`,
+          },
+          {
+            label: "student",
+            link: `/${base}/student`,
+          },
+        ]}
+      />
       {children}
     </Content>
   );
